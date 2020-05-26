691d8f97917bbfbc61e9fc5fd8edfcb105cc8d63
Teagan Keith <tokiyn@yahoo.com>
2020-05-26 00:41:44 -0400

    Added Nav and set state are top level of scope

--- my-dashboard/src/App.js
+++ my-dashboard/src/App.js
@@ -4,14 +4,35 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Plant from './components/Plant';
 import Login from './components/Login';
+import Signup from './components/SignUp'
 import PrivateRoute from './components/PrivateRoute';
 import UserCard from './components/UserCard';
 
+//Initial Login Values
+const initialLoginValues = {
+  username : "",
+  password : ""
+}
+
+const initialErrorValues ={
+  username: "",
+  password: "",
+}
+
 function App() {
+  //Login States
+const [loginValues, setLoginValues] = useState(initialLoginValues)
+const [formErrors, setFormErrors] = useState(initialErrorValues)
+
+
+
+
   return (
     <Router>
     <div className="App">
-      {/* <Route exact path='/' component={Login} /> */}
+      <Route exact path='/' component={Login}>
+      </Route>
+      <Route exact path='/signup' component={Signup}></Route>
       <PrivateRoute path='/dashboard' component={Dashboard} />
       <PrivateRoute path='/usercard' component={UserCard} />
     </div>
---
+++ my-dashboard/src/components/styledComponents/StyledDiv.js
@@ -0,0 +1,36 @@
+import React from "react"
+import styled from "styled-components"
+
+
+const StyledDiv = styled.div`
+    background-color: "#235B2D";
+    display:flex;
+    width: 100%;
+    color: white;
+    background-color: #235B2D;
+    height: 50px;
+    
+
+    
+
+    .header-nav{
+        display:flex;
+
+        a{
+            text-decoration:none;
+            color: white;
+            margin-left: 25px;
+            margin-top: 20px;
+         }
+
+        
+    }
+
+    
+    
+   
+
+`
+
+
+export default StyledDiv;
